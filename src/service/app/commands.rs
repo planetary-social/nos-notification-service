9582d76d3a906eff3b21cf3ff864e40833a43c0e
Matthew Remmel <matt.remmel@gmail.com>
2023-05-24 22:33:45 -0400

    Another use project Result for consistency

--- src/service/app/commands.rs
+++ src/service/app/commands.rs
@@ -1,7 +1,6 @@
 pub mod implementation;
 
-use std::error::Error;
-
+use crate::errors::Result;
 use crate::service::domain::Registration;
 
 pub struct Register {
@@ -9,5 +8,5 @@ pub struct Register {
 }
 
 pub trait RegisterHandler {
-    fn handle(&self, cmd: &Register) -> Result<(), Box<dyn Error>>;
+    fn handle(&self, cmd: &Register) -> Result<()>;
 }
